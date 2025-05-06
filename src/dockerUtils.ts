dd2c97f5d156cefada195512e4f714e4a939a8e9
Flavio Del Grosso <flaviodelgrosso@live.it>
2025-05-06 15:32:37 +0200

    fix: update logger import from .js to .ts in dockerUtils

--- src/dockerUtils.ts
+++ src/dockerUtils.ts
@@ -1,6 +1,6 @@
 import { exec } from 'child_process';
 import util from 'util';
-import { logger } from './logger.js';
+import { logger } from './logger.ts';
 
 const execPromise = util.promisify(exec);
 
